ac934316e299ec4d87ae967922cda03fe5ff0bdd
alex-hyp <alex.hyp.17@gmail.com>
2025-11-05 12:03:28 +0300

    add input file
    add ifstream
    
    add reading

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
 
@@ -7,6 +9,17 @@ int main()
 
     cout << endl << "Test" << endl;
 
+    ifstream F("input.txt");
+    
+    int x;
+    
+    while (!F.eof())
+    {
+        F >> x;
+        cout << x << endl;    /* code */
+    }
+    
+    
 
     return 0;
 }
